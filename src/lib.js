accab338816af875bfeca08410957926f0e955ae
Modesto Figuereo Fernandez <figuereo.fernandez.modesto@gmail.com>
2016-04-21 12:01:18 -0400

    Add changes to src/lib.js file.

--- src/lib.js
+++ src/lib.js
@@ -1,67 +1,61 @@
 function extend(receiver, giver, ...whitelist) {
-  let predicate = null;
-  if (whitelist.length !== 0) {
-    predicate = (property) => whitelist.indexOf(property) !== -1;
-  } else {
-    predicate = not((property) => hasOwnProperty(receiver, property));
-  }
+  const isInWhiteList = partial(isInArray, whitelist);
+  const doesReceiverNotHasProperty = negate(partial(hasOwnProperty, receiver));
+  const predicate = not(isArrayEmpty, whitelist) ? isInWhiteList : doesReceiverNotHasProperty;
 
-  const properties = keys(giver).filter(predicate);
-  if (isFunction(receiver)) {
-    if (isFunction(giver)) {
-      each(properties, (property) => {
-        receiver.prototype[property] = giver.prototype[property];
-      });
-    } else {
-      each(properties, (property) => {
-        receiver.prototype[property] = giver[property];
-      });
-    }
-  } else {
-    if (isFunction(giver)) {
-      each(properties, (property) => {
-        receiver[property] = giver.prototype[property];
-      });
-    } else {
-      each(properties, (property) => {
-        receiver[property] = giver[property];
-      });
-    }
-  }
+  const r = isFunction(receiver) ? receiver.prototype : receiver;
+  const g = isFunction(giver) ? giver.prototype : giver;
+  each(keys(giver).filter(predicate), (property) => {
+    r[property] = g[property];
+  });
 
   return receiver;
 }
 
-function hasOwnProperty(object, property) {
-  if (isFunction(object)) {
-    return Object.hasOwnProperty(object, property);
-  }
-
-  return Object.hasOwnProperty(object.prototype, property);
+function partial(fn, ...args) {
+  return (...moreArgs) => fn.apply({}, args.concat(moreArgs));
 }
 
-function keys(object) {
-  if (!object) return [];
-
-  if (isFunction(object)) {
-    return Object.keys(object.prototype);
-  }
-
-  return Object.keys(object);
+function negate(fn) {
+  return (...args) => !fn.apply(this, args);
 }
 
-function isFunction(object) {
-  return typeof object === 'function';
-}
-
-function not(expression) {
-  return (...args) => !expression.apply(this, args);
+function not(fn, ...args) {
+  return !fn.apply({}, args);
 }
 
 function each(collection, callback) {
   for (const item of collection) {
     callback(item);
   }
+}
+
+function isArrayEmpty(array) {
+  return array.length === 0;
+}
+
+function isInArray(array, item) {
+  return array.indexOf(item) !== -1;
+}
+
+function hasOwnProperty(object, property) {
+  if (isFunction(object)) {
+    return Object.hasOwnProperty(object.prototype, property);
+  }
+
+  return Object.hasOwnProperty(object, property);
+}
+
+function keys(object) {
+  if (isFunction(object)) {
+    return Object.keys(object.prototype);
+  }
+
+  return Object.keys(object || {});
+}
+
+function isFunction(object) {
+  return typeof object === 'function';
 }
 
 export default extend;
